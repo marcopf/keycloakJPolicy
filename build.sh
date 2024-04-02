3059528f32d3452972b6c5737e6a3c792b5461e5
marco <marco.paternosto@outlook.it>
2024-04-02 22:42:18 +0200

    build script improved

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
-if [ -f "policy.jar" ]; then
-  rm policy.jar
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <policy name> <outfile_name.jar>"
+    exit 1
 fi
-jar cf policy.jar META-INF policy.js
+
+cp -r META-INF_source META-INF
+sed -i "s/policyNameReplace/$1/g" META-INF/keycloak-scripts.json
+jar cf ./generated_policies/$2 META-INF policy.js
+if [ -d META-INF ]; then
+  rm -rf META-INF
+fi
