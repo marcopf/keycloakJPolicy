afe83b1a8f9bc7355679b61e04ddf3a1ec2deeea
marco <marco.paternosto@outlook.it>
2024-04-02 10:49:39 +0200

    policy implementation tested and working

--- policy.js
+++ policy.js
@@ -1,10 +1,10 @@
 //
 //https://www.keycloak.org/docs/24.0.1/authorization_services/#_policy_js
 //
-const context = $evaluation.getContext();
-const identity = context.getIdentity();
-const attributes = identity.getAttributes();
-const email = attributes.getValue('email').asString(0);
+var context = $evaluation.getContext();
+var identity = context.getIdentity();
+var attributes = identity.getAttributes();
+var email = attributes.getValue('email').asString(0);
 
 if (email.endsWith('@keycloak.org')) {
     $evaluation.grant();
